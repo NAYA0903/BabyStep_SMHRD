05243e56487d729c886881a961b2b3923b11acfd
ADMIN <ADMIN@DESKTOP-BVEL1G1>
2024-08-29 01:52:02 +0900

    Merge branch 'main' of https://github.com/NAYA0903/SMHRD_PROJECT.git into main

---
+++ MAIN PROJECT2/src/main/java/Servlet/MainServlet.java
@@ -0,0 +1,25 @@
+package Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// GET 요청이 들어오면 메인 페이지로 리디렉션
+			response.sendRedirect("Main.html");
+			
+		}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
