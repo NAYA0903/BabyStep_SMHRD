920ab83fe527db469150ee5f5f883fb74b65eb8a
fgh6605 <fgh6605@naver.com>
2024-09-19 15:25:41 +0900

    도감 수정

--- Ajang/Ajang/src/main/webapp/assets/js/News.js
+++ Ajang/Ajang/src/main/webapp/assets/js/News.js
@@ -1,16 +1,16 @@
 function openPopup(title, description) {
-    document.getElementById('popup-title').innerText = title;
-    document.getElementById('popup-description').innerText = description;
-    document.getElementById('popup').style.display = 'block';
+    document.getElementById('popup-title1').innerText = title;
+    document.getElementById('popup-description1').innerText = description;
+    document.getElementById('popup1').style.display = 'block';
 }
 
 function closePopup() {
-    document.getElementById('popup').style.display = 'none';
+    document.getElementById('popup1').style.display = 'none';
 }
 
 // 클릭 외부를 클릭 시 팝업 닫기
 window.onclick = function(event) {
-    const popup = document.getElementById('popup');
+    const popup = document.getElementById('popup1');
     if (event.target === popup) {
         closePopup();
     }
