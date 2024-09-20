916cd6720e68c220240278ffe4b993bf918370b6
sjw07028 <sjw07028@naver.com>
2024-09-20 09:42:56 +0900

    ekdldjfl

--- ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/controller/DiaryPopupServlet.java
+++ ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/controller/DiaryPopupServlet.java
@@ -11,29 +11,36 @@ import com.babystep.model.DiaryPopupDTO;
 
 public class DiaryPopupServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    // 서비스 인스턴스 생성
+    TodoService diaryService = new TodoService();
 
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // 서비스 인스턴스 생성
-        TodoService todoService = new TodoService();
+    	// 요청(Request) 인코딩 설정 (POST 방식)
+        request.setCharacterEncoding("UTF-8");
 
+        // 응답(Response) 인코딩 설정
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        
         // 선택한 날짜 가져오기
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
+     
+        // 선택된 날짜 생성
         String selectedDate = year + "-" + month + "-" + day;
-
+       
         // 세션에서 사용자 ID 가져오기
         String USER_ID = (String) request.getSession().getAttribute("id");
 
         // 날짜와 사용자 ID에 맞는 다이어리 목록 가져오기
-        List<DiaryPopupDTO> detaildiary = todoService.detaildiary(USER_ID, selectedDate);
+        List<DiaryPopupDTO> detaildiary = diaryService.detaildiary(USER_ID, selectedDate);
 
         // 데이터를 request 객체에 담아 JSP로 전달
         request.setAttribute("diaryList", detaildiary);
-
-        // 메인 JSP로 포워딩
         request.getRequestDispatcher("Main.jsp").forward(request, response);
+
     }
 }
--- ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/controller/TodoListServlet.java
+++ ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/controller/TodoListServlet.java
@@ -14,7 +14,7 @@ import com.babystep.model.TodoDTO;
 public class TodoListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	    private TodoService todoService = new TodoService();
+	    TodoService todoService = new TodoService();
 
 	    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	       
