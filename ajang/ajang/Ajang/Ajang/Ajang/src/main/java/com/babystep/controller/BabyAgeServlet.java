fa5daeae56bb70f5a38ff56245b07a3b13cc3dbd
sjw07028 <sjw07028@naver.com>
2024-09-25 09:26:24 +0900

    죄송해여 진짜 최종

--- ajang/ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/controller/BabyAgeServlet.java
+++ ajang/ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/controller/BabyAgeServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,9 +12,11 @@ import javax.servlet.http.HttpSession;
 
 import com.babystep.db.SqlSessionManager;
 import com.babystep.model.BabyDAO;
+import com.babystep.model.BabyDTO;
 import com.babystep.model.VaccineDAO;
 import com.babystep.model.VaccineDTO;
 
+import jakarta.websocket.Session;
 
 public class BabyAgeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
--- ajang/ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/model/VaccineDAO.java
+++ ajang/ajang/Ajang/Ajang/Ajang/src/main/java/com/babystep/model/VaccineDAO.java
@@ -1,5 +1,6 @@
 package com.babystep.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -12,13 +13,15 @@ public class VaccineDAO {
 	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
 	SqlSession sqlSession = sqlSessionFactory.openSession(true);
 	
-	public List<VaccineDTO> babyageselect(Integer babyage){
-		List<VaccineDTO> age = sqlSession.selectList("com.babystep.db.VaccineMapper.babyageselect" , babyage) ;
-		
-		sqlSession.close();
-		
-		return age ;
+	public List<VaccineDTO> babyageselect(Integer babyage) {
+	    if (babyage == null) {
+	        return new ArrayList<>(); // 또는 예외 처리
+	    }
+	    List<VaccineDTO> age = sqlSession.selectList("com.babystep.db.VaccineMapper.babyageselect", babyage);
+	    sqlSession.close();
+	    return age;
 	}
+
 	
 
 }
